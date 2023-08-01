338ee5e46551e60b393285c9b1156caad5e5e250
MucahidDeveloper <127043807+MucahidDeveloper@users.noreply.github.com>
2023-08-01 18:31:41 +0300

    Update Favorite-Cities-Challenge.py

--- 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/Favorite-Cities-Challenge.py
+++ 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/Favorite-Cities-Challenge.py
@@ -1,7 +1,7 @@
-favFood = "chicken"
-answer = input("What is my favourite food?")
-if answer == favFood:
-  print("Yeah!! You're right! My favroute food is ", favFood)
+favCity = "Makka"
+answer = input("What is my favourite city?")
+if answer == favCity:
+  print("Yeah!! You're right! My favroute City is ", favCity)
 else:
-  print("That is wrong! My favourite food isn't ", answer)
+  print("That is wrong! My favourite City isn't ", answer)
   print("See you!")
