b66538084ce47071209e585d703953bff114e84e
MucahidDeveloper <127043807+MucahidDeveloper@users.noreply.github.com>
2023-08-01 18:47:10 +0300

    Update Strings-challenge.py

--- 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challenges/Strings-challenge.py
+++ 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challenges/Strings-challenge.py
@@ -1,1 +1,22 @@
+def Mile ():
+    miles = input('Mil cinsinden bir mesafe girin: ')
+    # kilometers_value = miles_value * 1.60934
+    
+    y = float(miles)
 
+    
+    print("Bu eşittir" , y * 1.609344 , 'km')
+
+
+    print("1 miles = 1.609344 kilometers; Sadece MATH yaptık!")
+    
+
+    reput = input ('Başka bir mesafe hesap istiyor musun? ')
+    if reput == 'Hayır':
+        print('Tembel seçim!')
+    else:
+        Mile ()
+
+Mile ()
+
+print('İyi zamanlar!')
